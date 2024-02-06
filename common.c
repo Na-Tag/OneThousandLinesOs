3efc5423a073c82c4bc2f7a551c7dafe76239f24
naoki_taguchi <puittu3450nao@outlook.jp>
2024-02-06 12:12:47 +0900

    creating c standard library

--- common.c
+++ common.c
@@ -1,5 +1,49 @@
 #include "common.h"
 
+// メモリ操作の関数
+/// bufの先頭からnバイト分をcで埋める。
+void *memset(void *buf, char c, size_t n)
+{
+    uint8_t *p = (uint8_t *)buf;
+    while (n--)
+    {
+        *p++ = c; // *p++ = c ->  *p = c, p++;
+    }
+    return buf;
+}
+
+/// srcからnバイト分をdstにコピーする。
+void *memcpy(void *dst, const void *src, size_t n)
+{
+    uint8_t *d = (uint8_t *)dst;
+    const uint8_t *s = (const uint8_t *)src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+// メモリ操作の関数
+
+// 文字列操作の関数
+char *strcpy(char *dst, const char *src)
+{
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+int strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2)
+    {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+// 文字列操作の関数
 void putchar(char ch);
 
 void printf(const char *fmt, ...)
@@ -55,6 +99,8 @@ void printf(const char *fmt, ...)
                 int value = va_arg(vargs, int);
                 for (int i = 7; i >= 0; i--)
                 {
+                    // 0xで16進数を表す。(0始まりで８進数を表現できる。)
+                    // &演算子はAND計算を行う。3&2は、011&010=010より、2となる。
                     int nibble = (value >> (i * 4)) & 0xf;
                     putchar("0123456789abcdef"[nibble]);
                 }
--- kernel.c
+++ kernel.c
@@ -36,14 +36,6 @@ void putchar(char ch)
 {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /*Console Putchar extension*/);
 }
-void *
-memoryset(void *buf, char c, size_t n)
-{
-    uint8_t *p = (uint8_t *)buf;
-    while (n--)
-        *p++ = c;
-    return buf;
-}
 
 void kernel_main(void)
 {
