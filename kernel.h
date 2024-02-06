89d3ab1929e251e625fb9225b637b6be7251db26
naoki_taguchi <puittu3450nao@outlook.jp>
2024-02-06 16:02:06 +0900

    creating kernel panick macro

--- kernel.h
+++ kernel.h
@@ -1,5 +1,16 @@
 #pragma once
 
+// kernelパニック
+// エラー内容を表示した後に無限ループに入って処理を停止する。
+#define PANIC(fmt, ...)                                                       \
+    do                                                                        \
+    {                                                                         \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        while (1)                                                             \
+        {                                                                     \
+        }                                                                     \
+    } while (0)
+
 struct sbiret
 {
     long error;
